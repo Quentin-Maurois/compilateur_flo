6de008c4fea4aa3a8d1de8b50d5bf967ce93b384
Quentin-Maurois <quentin.maurois@etu.univ-cotedazur.fr>
2023-04-14 15:21:46 +0200

    analyse lexicale

--- analyse_lexicale.py
+++ analyse_lexicale.py
@@ -3,12 +3,14 @@ from sly import Lexer
 
 class FloLexer(Lexer):
 	# Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
-	tokens = { IDENTIFIANT, ENTIER, ECRIRE,INFERIEUR_OU_EGAL }
+	tokens = { IDENTIFIANT, ENTIER, ECRIRE, INFERIEUR_OU_EGAL, SI, SINON, BOOLEAN, TANTQUE}
+
+	# SINON, SI, BOOLEAN, TANTQUE
 
 	#Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale. 
 	#Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
 	#Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
-	literals = { '+','*','(',')',";" }
+	literals = { '+','*','(',')',";",",","{","}","=",">","<","%","-","/","!" }
 	
 	# chaines contenant les caractère à ignorer. Ici espace et tabulation
 	ignore = ' \t'
